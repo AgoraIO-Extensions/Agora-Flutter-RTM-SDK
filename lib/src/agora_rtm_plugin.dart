f65a007b573bd8dad96e4f60c8bd6c8dad87061d
Max Cobb <maxxc@mac.com>
2022-04-20 17:37:44 +0200

    update package contents to pass all pub.dev\'s checks

--- lib/src/agora_rtm_plugin.dart
+++ lib/src/agora_rtm_plugin.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/services.dart';
 
 mixin AgoraRtmPlugin {
-  static const MethodChannel _methodChannel = const MethodChannel("io.agora.rtm");
+  static const MethodChannel _methodChannel =
+      const MethodChannel("io.agora.rtm");
 
-  static Future<dynamic> _sendMethodMessage(String call, String method, Map? arguments) {
-    return _methodChannel.invokeMethod(method, {"call": call, "params": arguments});
+  static Future<dynamic> _sendMethodMessage(
+      String call, String method, Map? arguments) {
+    return _methodChannel
+        .invokeMethod(method, {"call": call, "params": arguments});
   }
 
   static Future<dynamic> callMethodForStatic(String name, Map? arguments) {
