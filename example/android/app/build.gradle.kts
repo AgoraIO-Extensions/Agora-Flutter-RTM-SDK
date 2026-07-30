99974634f0847785d16999004d9a6dea59134b74
Arthas <luxuhui@agora.io>
2026-07-30 18:07:48 +0800

    fix(example): align with Flutter 3.29.3 template

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "io.agora.agora_rtm_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
